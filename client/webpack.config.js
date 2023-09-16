91e2176566d7362ceec0f111dbc9ad92c79bdc22
Yajiit <tjagerter@gmail.com>
2023-09-16 07:22:36 -0400

    added workbox plugins for service worker and manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// COMPLETED: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -18,9 +18,29 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './client/index.html',
+        filename: 'index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor for notes and code snippets.',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#1e88e5',
+        icons: [
+          {
+            src: path.resolve('client/assets/icons/icon_96x96.png'),
+            sizes: [72, 96, 128, 144, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './client/src-sw.js',
+        swDest: 'sw.js',
+      }),
     ],
-
     module: {
       rules: [
         
