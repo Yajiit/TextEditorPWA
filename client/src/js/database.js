40f35734f7a0f475866f7c25478c76c3531786a5
Yajiit <tjagerter@gmail.com>
2023-09-16 07:36:47 -0400

    configured indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,19 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readwrite');
+  const jateStore = transaction.objectStore('jate');
+  await jateStore.add({ content });
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readonly');
+  const jateStore = transaction.objectStore('jate');
+  const data = await jateStore.getAll();
+  return data[0] ? data[0].content : null;
+};
 
 initdb();
